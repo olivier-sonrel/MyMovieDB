43dd7e868c0cf9b8eee4940db3725cec34f90716
olivier-sonrel <o.sonrel@it-students.fr>
2021-05-20 17:12:59 +0300

    tente affiche quand deja dans watch list

--- src/Controller/Admin/Crud/ActorCrudController.php
+++ src/Controller/Admin/Crud/ActorCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin\Crud;
 
 use App\Entity\Actor;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ActorCrudController extends AbstractCrudController
 {
@@ -12,14 +15,15 @@ class ActorCrudController extends AbstractCrudController
         return Actor::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('firstName'),
+            TextField::new('lastName'),
+            ImageField::new('image')->setUploadDir("/public/assets/upload/image")
+                ->setBasePath("assets/upload/image"),
         ];
     }
-    */
+
 }
